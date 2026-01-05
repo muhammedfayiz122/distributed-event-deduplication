c681e02d8f56dc96e0f96248644293f3c47e2744
muhammedfayiz122 <muhammedfayiz122@gmail.com>
2026-01-05 21:19:48 +0530

    Phase 2 done. Event model + Pydantic validation completed.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI, WebSocket
+from pydantic import ValidationError
 from app.config import settings
+from app.utils.logger import CustomLogger
+from app.schemas.event_schema import EventSchema
+# from app.utils.redis_client import get_redis_client
+import json
+
+logger = CustomLogger().get_logger(__name__)
 
 app = FastAPI(
     title=settings.app_name,
@@ -14,6 +21,15 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     
     while True:
-        event = await websocket.receive_text()
-        print(f"Received event: {event}")
-        await websocket.send_text(f"Event received: {event}")
+        try:
+            raw_data = await websocket.receive_text()
+            event = EventSchema(**json.loads(raw_data))
+            logger.info("Received event", event_id=event.event_id, event_type=event.event_type)
+        except ValidationError as e:
+            error_msg = f"Invalid event format received: {e.errors()}"
+            logger.error(error_msg)
+            await websocket.send_text(error_msg)
+        except Exception as e:
+            error_msg = f"An error occurred: {str(e)}"
+            logger.error(error_msg)
+            await websocket.send_text(error_msg)
---
+++ app/schemas/event_schema.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import Optional, Dict, Any
+
+class EventSchema(BaseModel):
+    event_id: str = Field(...,  description="Unique identifier for the event", min_length=1)
+    event_type: str = Field(..., description="Type of the event")
+    payload: Dict[str, Any] = Field(None, description="Optional payload containing event data")
+    created_at: datetime = Field(..., description="Timestamp when the event was created")
+    
+
